bb2459837d851b73a971bf6f54a736505d85283f
Arthur Brunet <arthur.brunet76@gmail.com>
2022-07-12 14:05:48 +0200

    fix merge

--- src/components/Home.js
+++ src/components/Home.js
@@ -130,26 +130,39 @@ export default function Home() {
 
     return (
         <div style={styles.columns}>
-            <div style={{...styles.row, marginTop:0}}>
-                <Square icon={"/euro.svg"} title="Achat"
-                        text="Estimation de la variation des recettes par comparaison à 2020: "
-                        color="rgba(61,212,71,35%)" titleColor="rgb(61,212,71)" data="Environ +3.7M en cumulé à fin aout"/>
-                <Square icon={"/stats.svg"} title="Commercial"
-                        text="Estimation de la variation des recettes par comparaison à 2019: "
-                        color="rgba(229,175,47,20%)" titleColor="rgb(229,175,47)" data="Environ -3.7M en cumulé à fin aout"/>
-                <Square icon={"/user.svg"} title="Ressources humaines"
-                        text="Êtres en conformité avec la réglementation :" titleColor="rgb(199,122,140)" color="rgba(199,122,140,20%)"/>
+            <div style={styles.row}>
+                <Link style={{ textDecoration: 'none' }} to="/detailPurchase">
+                    <Square icon={"/euro.svg"} title="Achat"
+                            text="Nombre de ticket total vendu : "
+                            color="rgba(61,212,71,35%)" titleColor="rgb(61,212,71)" data={nbTotalVendu}/>
+                </Link>
+                <Link style={{ textDecoration: 'none' }} to="/detailsCommercial">
+                    <Square icon={"/stats.svg"} title="Commercial"
+                            text="Recette global :"
+                            color="rgba(229,175,47,20%)" titleColor="rgb(229,175,47)" data={recetteGlobale}/>
+                </Link>
+                <Link style={{ textDecoration: 'none' }} to="/detailsHumanRessource">
+                    <Square icon={"/user.svg"} title="Ressources humaines"
+                            text="Taux d'absenteisme moyen :" data={tauxAbsenteisme} titleColor="rgb(199,122,140)"
+                            color="rgba(199,122,140,20%)"/>
+                </Link>
             </div>
-            <div style={{...styles.row, marginBottom:0}}>
-                <Square icon={"/gears.svg"} title="Technique" text="Voyages lignes régulières en 2021"
-                        titleColor="rgb(247,209,136)" color="rgba(247,209,136,20%)" data="26821899"/>
-                <Square icon={"/ticket.svg"} title="Exploitation" text="Tcar - Kilomètres métro haut le pied en 2021:"
-                        titleColor="rgb(121,176,224)"
-                        color="rgba(121,176,224,20%)" data="48170"/>
-                <Square icon={"/leaf.svg"} title="Environnement"
-                        text="Navette fluviale à énergie électro-solaire.Nombre de passagers en janvier 2021"
-                        titleColor="rgb(212,228,150)"
-                        color="rgba(212,228,150,20%)" data="6563"/>
+            <div style={styles.row}>
+                <Link style={{ textDecoration: 'none' }} to="/detailTechnical">
+                    <Square icon={"/gears.svg"} title="Technique" text="Taux de panne moyen des bus et TEOR"
+                            titleColor="rgb(247,209,136)" color="rgba(247,209,136,20%)" data={tauxPanne}/>
+                </Link>
+                <Link style={{ textDecoration: 'none' }} to="/detailsExploitation">
+                    <Square icon={"/ticket.svg"} title="Exploitation" text="Nombre total d'accidents:" data={nbAccident}
+                            titleColor="rgb(121,176,224)"
+                            color="rgba(121,176,224,20%)" data="48170"/>
+                </Link>
+                <Link style={{ textDecoration: 'none' }} to="/detailsEnvironment">
+                    <Square icon={"/leaf.svg"} title="Environnement"
+                            text="Impact carbone :"
+                            titleColor="rgb(212,228,150)"
+                            color="rgba(212,228,150,20%)" data={impactCarbone}/>
+                </Link>
             </div>
         </div>
     );
